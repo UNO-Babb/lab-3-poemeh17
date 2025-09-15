5d99a636c45bec08cac99cf15d74a60f6a81c0dd
poemeh17 <poem3h@gmail.com>
2025-09-15 00:46:27 +0000

    Temperature Converted

--- ApproxPi.py
+++ ApproxPi.py
@@ -12,15 +12,26 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
-
+  precision = int(input("Please enter the desired decimal precision (up to 10): "))
+  print(f"How many decimal points to compute (0-10): {precision}")
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
+  approxPi = 4/1
+  sign = -1
+  denom = 3
+  while round(approxPi, precision) != round(realPi, precision):
+    #print(approxPi)
+    approxPi = approxPi + (sign *4/ denom)
+
+
+    sign = sign * -1
+    denom = denom + 2
 
   end = time.time()
 
   elapsedTime = end - start
-  print(elapsedTime)
+  print(f"Calculated in {elapsedTime} secounds.")
 
 if __name__ == '__main__':
   main()
